932423490ea666986beb4cef271896dfcb5ce4f0
samlafontaine <sam@samlafontaine.com>
2024-02-16 00:11:19 +0100

    main styles desktop

---
+++ app/fonts.tsx
@@ -0,0 +1,13 @@
+import { Roboto, Rozha_One } from "next/font/google";
+
+export const roboto = Roboto({
+  weight: ["400", "900"],
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export const rozha_one = Rozha_One({
+  weight: ["400"],
+  subsets: ["latin"],
+  display: "swap",
+});
